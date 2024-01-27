e66e8ae4bfa6e02bc586d1dbe5cfd43d7550f1eb
Igor Verkhov <iVerkhov@icoud.com>
2024-01-27 16:05:25 +0300

    Initial Commit

---
+++ ContactsList/ContactsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListApp.swift
+//  ContactsList
+//
+//  Created by Игорь Верхов on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
